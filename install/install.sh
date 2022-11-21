47912a5bc1a9aac2be2801e6706757aa076392dc
Boris Rorsvort <boris@rorsvort.com>
2022-11-21 12:28:00 +0100

    fix

--- install/install.sh
+++ install/install.sh
@@ -2,12 +2,16 @@
 # Setup macos defaults and add apps to dock
 # . "$DOTFILES_DIR/macos/defaults.sh"
 # . "$DOTFILES_DIR/macos/dock.sh"
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 # Clear cache
-. "~/.dotfiles/bin/dotfiles" clean
+. "$DOTFILES_DIR/bin/dotfiles" clean
+# Clear cache
+. "$DOTFILES_DIR/bin/dotfiles" symlinks
 
 # Replace default hosts file
-. "~/.dotfiles/install/hosts.sh"
+. "$DOTFILES_DIR/install/hosts.sh"
 
 # Add keys from keychain to ssh agent
 ssh-add -A 2>/dev/null;
