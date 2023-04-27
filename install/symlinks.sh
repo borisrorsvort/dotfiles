1d2de53ff7fa5f07919b334607752064874841e1
Boris Rorsvort <boris@rorsvort.com>
2023-04-27 10:22:25 +0200

    prepare for nvim config

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -1,5 +1,5 @@
 # ln -s "$HOME/dotfiles/dots/.gitconfig" ~
 # ln -s "$HOME/dotfiles/dots/.gitignore_global" ~
 # ln -s "$HOME/dotfiles/dots/.zshrc" ~
-ln -s "$HOME/dotfiles/lvim/config.lua" "$HOME/.config/lvim"
+ln -s "$HOME/dotfiles/nvim/" "$HOME/.config/nvim/lua/user/"
 # ln -s "$(which node)" /System/Volumes/Data/usr/local/bin
