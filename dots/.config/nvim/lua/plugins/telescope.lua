e8d37772daa03a785ac154f65713831991c4ed68
Boris Rorsvort <boris@rorsvort.com>
2026-08-31 17:58:15 +0200

    refactor: show hidden files in telescope

--- dots/.config/nvim/lua/plugins/telescope.lua
+++ dots/.config/nvim/lua/plugins/telescope.lua
@@ -26,5 +26,15 @@ return {
         },
       },
     },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+      live_grep = {
+        additional_args = function()
+          return { "--hidden" }
+        end,
+      },
+    },
   },
 }
